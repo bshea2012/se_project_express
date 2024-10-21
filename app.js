c007966d79f1f731900060c9ef36400042335220
Bshea12 <brogenshea@gmail.com>
2024-10-21 18:31:50 -0500

    crash protection

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ mongoose
 app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", indexRouter);
 app.use(errorLogger);
 app.use(errors());
